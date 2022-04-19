6a91fadf9664b0934303aecc83268045d380abad
tkhs1121 <tkhs1121@protonmail.com>
2022-04-19 13:30:21 +0900

    ランダムでエラーレスポンスを返却するように変更

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-from flask import Flask
+from random import randint
+from flask import Flask, Response
 
 PORT = 8080
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-  print("hello world!")
+    r = randint(0, 2)
+    if r == 0:
+        return Response(status=200)
+    if r == 1:
+        return Response(status=400)
+    if r == 2:
+        return Response(status=500)
 
 if __name__ == "__main__":
-  app.run(debug=True, host="0.0.0.0", port=PORT)
+    app.run(debug=True, host="0.0.0.0", port=PORT)
